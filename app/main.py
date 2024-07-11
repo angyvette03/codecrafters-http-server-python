de005fcd05efeb73fc53c7d564dccb9b5c499e08
yvette <angyvette03@gmail.com>
2024-07-11 22:56:21 +0800

    pass stage

--- app/main.py
+++ app/main.py
@@ -5,12 +5,34 @@ import socket
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
+    
+    def parse_request(request):
+        lines = request.split("\r\n")
+        method, path, version = lines[0].split(" ")
+        return method, path, version
+    
+    def response(path):
+        responses = {
+            '/': "HTTP/1.1 200 OK\r\n\r\n"
+        }
+        
+        default_response = "HTTP/1.1 404 Not Found\r\n\r\n"
+        
+        responses.get(path, default_response)
+    
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    # server_socket.accept() # wait for client
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    server_socket.listen() 
+    
+    while True:
+        client_socket, client_address = server_socket.accept()
+        data = client_socket.recv(1024)
+        method, path, version = parse_request(data)
+        http_response = response(path)
+        client_socket.sendall(http_response.encode())
+        client_socket.close()
+        
+        
+    
     
 
 

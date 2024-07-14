8684245e4ab82ab3da7bf91f69f94c6e186dbaa8
yvette <angyvette03@gmail.com>
2024-07-14 22:16:38 +0800

    pass stage

--- app/main.py
+++ app/main.py
@@ -12,15 +12,17 @@ def main():
         splitPath = path.split("/")
         if len(splitPath) > 2:
             res = splitPath[2]
+            isEcho = True
         else:
             res = splitPath[1]
-        return method, path, version, res
+            isEcho = False
+        return method, path, version, res, isEcho
     
-    def response(res):
-        resType = 'text/plain'
-        length = str(len(res))
-        body = str(res)
-        return (f"HTTP/1.1 200 OK\r\nContent-Type: {resType}\r\nContent-Length: {length}\r\n\r\n{body}").encode()
+    # def response(res):
+    #     resType = 'text/plain'
+    #     length = str(len(res))
+    #     body = str(res)
+    #     return (f"HTTP/1.1 200 OK\r\nContent-Type: {resType}\r\nContent-Length: {length}\r\n\r\n{body}").encode()
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.listen() 
@@ -28,12 +30,15 @@ def main():
     while True:
         client_socket, client_address = server_socket.accept()
         data = client_socket.recv(1024).decode()
-        method, path, version, res = parse_request(data)
-    
-        http_response = response(res)
+        method, path, version, res, isEcho = parse_request(data)
+        resType = 'text/plain'
+        length = str(len(res))
+        body = str(res)
+        if isEcho:
+            http_response = (f"HTTP/1.1 200 OK\r\nContent-Type: {resType}\r\nContent-Length: {length}\r\n\r\n{body}").encode()
+        else:
+            http_response = (f"HTTP/1.1 404 Not Found\r\nContent-Type: {resType}\r\nContent-Length: {length}\r\n\r\n{body}").encode()  
         client_socket.sendall(http_response)
         client_socket.close()
-        
-
 if __name__ == "__main__":
     main()

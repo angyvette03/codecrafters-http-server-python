4abcc59b634f51ea3eb9b3b85cbf352b68ef7d90
yvette <angyvette03@gmail.com>
2024-07-14 22:04:32 +0800

    pass stage

--- app/main.py
+++ app/main.py
@@ -9,16 +9,22 @@ def main():
     def parse_request(request):
         lines = request.split("\r\n")
         method, path, version = lines[0].split(" ")
-        return method, path, version
+        splitPath = path.split("/")
+        res = splitPath[1]
+        return method, path, version, res
     
-    def response(path):
-        responses = {
-            '/': "HTTP/1.1 200 OK\r\n\r\n".encode()
-        }
+    def response(res):
+        # responses = {
+        #     '/': "HTTP/1.1 200 OK\r\n\r\n".encode()
+        # }
         
-        default_response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
+        # default_response = "HTTP/1.1 404 Not Found\r\n\r\n".encode()
         
-        return responses.get(path, default_response)
+        # return responses.get(path, default_response)
+        resType = 'text/plain'
+        length = str(len(res))
+        body = str(res)
+        return (f"HTTP/1.1 200 OK\r\nContent-Type: {resType}\r\nContent-Length: {length}\r\n\r\n{body}").encode()
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     server_socket.listen() 
@@ -26,8 +32,9 @@ def main():
     while True:
         client_socket, client_address = server_socket.accept()
         data = client_socket.recv(1024).decode()
-        method, path, version = parse_request(data)
-        http_response = response(path)
+        method, path, version, res = parse_request(data)
+    
+        http_response = response(res)
         client_socket.sendall(http_response)
         client_socket.close()
         

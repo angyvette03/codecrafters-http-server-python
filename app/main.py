6c3dd2c133fcfc0fd3f57556c28909354e2fde12
yvette <angyvette03@gmail.com>
2024-07-20 22:29:53 +0800

    pass stage

--- app/main.py
+++ app/main.py
@@ -25,6 +25,7 @@ def main():
                 return (f"HTTP/1.1 200 OK\r\nContent-Type: {resType}\r\nContent-Length: {length}\r\n\r\n{userAgent}").encode()
             elif ("echo" in path):
                 acceptEncodingHeader = request.split("\r\n")[1]
+                print(acceptEncodingHeader)
                 encodingType = acceptEncodingHeader.split(" ")[1]
                 if(encodingType == 'gzip'):
                     return (f"HTTP/1.1 200 OK\r\nContent-Type: {resType}\r\nContent-Encoding: {encodingType}").encode()

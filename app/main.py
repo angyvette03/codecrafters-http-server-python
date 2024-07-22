4c4dcc4cd3958c8a6fc417caf534a31cde1f1120
yvette <angyvette03@gmail.com>
2024-07-22 23:12:18 +0800

    pass stage

--- app/main.py
+++ app/main.py
@@ -41,13 +41,13 @@ def main():
                         f"\r\n"
                     ).encode() + compressedBody
                     return response
-                elif(acceptEncodingHeader == 'invalid-encoding'):
-                # else:
-                    return (f"HTTP/1.1 200 OK\r\nContent-Type: {resType}\r\n\r\n{len(path.split('/')[2])}").encode()
+                # elif(acceptEncodingHeader == 'invalid-encoding'):
+                # # else:
+                #     return (f"HTTP/1.1 200 OK\r\nContent-Type: {resType}\r\n\r\n{len(path.split('/')[2])}").encode()
                 else:
                     print('hello world')
                     
-                    return (f"HTTP/1.1 200 OK\r\nContent-Type: {resType}\r\nContent-Length: {len(path.split('/')[2])}\r\n\r\n").encode()
+                    return (f"HTTP/1.1 200 OK\r\nContent-Type: {resType}\r\n\r\nContent-Length: {len(path.split('/')[2])}\r\n\r\n").encode()
             elif (path.startswith('/files')):
                 directory = sys.argv[2]
                 filename = path[7:]
